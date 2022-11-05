78a39e3ebaba8a58be630e735e198b47997085d6
ansattz <dvieiras@aol.com>
2022-11-04 23:42:11 -0300

    button para loc; themes ainda em teste; tudo funcionando com matchPlaylist()

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-const pegarLocalizacao = () => {
+var latitude, longitude, dadosLocalizacao, tempo;
+let btnLoc = document.getElementById('btnLoc')
+let playSec = document.getElementById('playlist-id')
+
+const themes = {
+   Clouds: {
+      'few clouds': "7LOpKH4CjlNkIRwXr2vcHf",
+      'scattered clouds': "",
+      'broken clouds': "532Fg95iIa7PF3ulFGrKfv",
+      'overcast clouds': "5C1OsolkEdvyrboc25OLG8"
+   },
+   Rain: {
+      'light rain': "47PtuZO5RI7lLAV2ldBbld",
+      'moderate rain': "4QAYAMQrGEG6u8OaopJTxX",
+      'shower rain': "4GmPOIOCItFa4TtFmG7Ysz"
+   }
+}
+
+function criarPlaylist(playlist_id) {
+   const playlistDiv = document.createElement("div")
+   var conteudo =
+      `
+      <iframe style="border-radius:12px" 
+         src="https://open.spotify.com/embed/playlist/${playlist_id}?utm_source=generator" 
+         width="35%" height="450" frameBorder="0" allowfullscreen="" 
+         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+         loading="lazy">
+      </iframe>
+   `
+   playlistDiv.innerHTML = conteudo
+   return playlistDiv
+}
+
+btnLoc.onclick = () => {
    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(mostrarPosicao);
+      navigator.geolocation.getCurrentPosition(mostrarPosicao, erroPosicao)
    } else {
       alert("Geolocation nao suportado.")
    }
 }
+
 const mostrarPosicao = (pos) => {
-   var latitude = pos.coords.latitude
-   var longitude = pos.coords.longitude
-   return [latitude, longitude]
+   latitude = pos.coords.latitude
+   longitude = pos.coords.longitude
+}
+
+const erroPosicao = (erro) => {
+   alert("Geolocation nao pegou loc")
 }
 
 const httpGet = (url) => {
@@ -18,6 +55,18 @@ const httpGet = (url) => {
    return JSON.parse(http.responseText)
 }
 
-// httpGet("https://nominatim.openstreetmap.org/reverse?lat=" + latitude + "&lon=" + longitude + "&format=json")
+const pegarDadosLocalizacao = () => {
+   dadosLocalizacao = httpGet("https://nominatim.openstreetmap.org/reverse?lat=" + latitude + "&lon=" + longitude + "&format=json")
+}
 
+const pegarTempo = () => {
+   tempo = httpGet("https://api.openweathermap.org/data/2.5/weather?q=" +
+      dadosLocalizacao.address.city + "&appid=2b425334302e4882839efd307f61b3fa")
+}
+
+const matchPlaylist = () => {
+   pegarDadosLocalizacao()
+   pegarTempo()
+   playSec.appendChild(criarPlaylist(themes[tempo.weather[0].main][tempo.weather[0].description]))
+}
 
