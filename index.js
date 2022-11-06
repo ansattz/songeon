1dd81eb834f8d1fa0d442d66b45cdfa2dd1b93c2
ansattz <dvieiras@aol.com>
2022-11-06 18:12:11 -0300

    valores mp4 adicionados; tentando fazer com que o mp4 de background varie com o botao de loc

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var latitude, longitude, dadosLocalizacao, tempoInfo;
 let btnLoc = document.getElementById('btnLoc')
-let playSec = document.getElementById('playlist-id')
-
+let corpo = document.getElementById('corpo-conteudo')
+let gitBtn = document.getElementById('autor-id')
+let lofiBtn = document.getElementById('lofi-id')
+let classicBtn = document.getElementById('classic-id')
+let deepBtn = document.getElementById('deeph-id')
+let ambientBtn = document.getElementById('ambient-id')
 // Teremos uma playlist de acordo com o tempo na regiao.
 // A descricao nos entrega mais detalhes sobre como esta o 
 // tempo naquela regiao. Como sendo a primeira versao 
 // essa eh uma solucao razoavel antes de pensar em como 
 // esta a temperatura na regiao.
+
 const theme = {
    "Clouds": {
       "few clouds": "1J58o8yP11hcw0ZiJ3Q9mQ",
@@ -21,21 +26,21 @@ const theme = {
       "heavy intensity rain": "5C1OsolkEdvyrboc25OLG8"
    }
 }
-const bgs = {
-   'few clouds': "https://wallpaperaccess.com/full/792.jpg",
-   'mist': "https://media.tenor.com/B4EqOMlV0-wAAAAC/aesthetic-wallpaper.gif",
-   'moderate rain': "https://media.tenor.com/hXJOs4hRlFQAAAAC/kikis-delivery-service-city.gif",
-   'heavy intensity rain': "https://media.tenor.com/hXJOs4hRlFQAAAAC/kikis-delivery-service-city.gif",
-   'very heavy rain': "https://media.tenor.com/hXJOs4hRlFQAAAAC/kikis-delivery-service-city.gif",
-   'extreme rain': "https://acegif.com/wp-content/uploads/rain-25.gif",
-   'scattered clouds': "https://i0.wp.com/windowscustomization.com/wp-content/uploads/2018/12/Anime-And-Scape-Watefal.gif?resize=750%2C356&quality=80&strip=all&ssl=1"
-}
 
-function configBg() {
-   document.body.style["-webkit-background-size"] = "cover"
-   document.body.style["-moz-background-size"] = "cover"
-   document.body.style["-o-background-size"] = "cover"
-   document.body.style["background-size"] = "cover"
+const bgs = {
+   'few clouds': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Chilling-with-My-Cat.mp4",
+   'broken clouds': "https://mylivewallpapers.com/wp-content/uploads/Fantasy/PREVIEW-Up-In-the-Clouds.mp4",
+   'overcast clouds': "https://mylivewallpapers.com/wp-content/uploads/Fantasy/PREVIEW-Boat-on-Clouds.mp4",
+   'mist': "https://mylivewallpapers.com/wp-content/uploads/Nature/PREVIEW-Calm-Lake.mp4",
+   'moderate rain': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Living-Room-Chill.mp4",
+   'heavy intensity rain': "https://mylivewallpapers.com/wp-content/uploads/City/PREVIEW-Rain-Street-Ghostwire-Tokyo.mp4",
+   'very heavy rain': "https://mylivewallpapers.com/wp-content/uploads/City/PREVIEW-Raining-Streets-GTA5.mp4",
+   'extreme rain': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Raining-Japanese-Village.mp4",
+   'scattered clouds': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Room-with-Candles.mp4",
+   'lofi': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Lo-Fi-Coffee-Shop.mp4",
+   'deeph': "https://mylivewallpapers.com/wp-content/uploads/Fantasy/PREVIEW-Moon-Pool-Party.mp4",
+   'classic': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Study-Session.mp4"
+
 }
 
 function criarPlaylist(playlist_id) {
@@ -44,7 +49,7 @@ function criarPlaylist(playlist_id) {
       `
       <iframe style="border-radius:12px" 
          src="https://open.spotify.com/embed/playlist/${playlist_id}?utm_source=generator&theme=0" 
-         width="100%" height="380" frameBorder="0" allowfullscreen="" 
+         width="100%" height="380" frameBorder="0" 
          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
          loading="lazy">
       </iframe>
@@ -59,10 +64,6 @@ btnLoc.onclick = () => {
    } else {
       alert("Geolocation nao suportado.")
    }
-}
-
-function temaBg(wallpaper) {
-   document.body.style.background = `url(${wallpaper}) no-repeat center center fixed`
 }
 
 const mostrarPosicao = (pos) => {
@@ -92,20 +93,23 @@ const pegarTempo = () => {
 }
 
 function aplicarTema() {
-   const bodySel = document.querySelector('body')
    pegarDadosLocalizacao()
    pegarTempo()
    var tempoAtual = tempoInfo.weather[0].main
    var descTempo = tempoInfo.weather[0].description
    // Por enquanto acho interessante, quando pegar a localizacao novamente, 
    // "atualizar" a playlist
-   if (getComputedStyle(bodySel).background.includes("bg.jpeg") == true) {
-      playSec.appendChild(criarPlaylist(theme[tempoAtual][descTempo]))
+
+   if (document.getElementById('bg-id').src.includes("Fisherman") == true) {
+      corpo.appendChild(criarPlaylist(theme[tempoAtual][descTempo]))
    }
    else {
-      playSec.removeChild(playSec.firstElementChild)
-      playSec.appendChild(criarPlaylist(theme[tempoAtual][descTempo]))
+      corpo.removeChild(corpo.firstElementChild)
+      corpo.appendChild(criarPlaylist(theme[tempoAtual][descTempo]))
    }
-   temaBg(bgs[descTempo])
-   configBg()
+   document.getElementById('bg-id').src = bgs[descTempo]
+}
+
+gitBtn.onclick = () => {
+   window.open("https://github.com/ansattz", "_blank")
 }
