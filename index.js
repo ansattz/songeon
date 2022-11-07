a1b3248b4c9ce5914725c7694714ddf43baa7e90
ansattz <dvieiras@aol.com>
2022-11-07 00:40:25 -0300

    radio adicionada

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var latitude, longitude, dadosLocalizacao, tempoInfo;
+var playerDeeph, playerLofi, playerClassic, playerAmbient;
 let btnLoc = document.getElementById('btnLoc')
 let corpo = document.getElementById('corpo-conteudo')
 let gitBtn = document.getElementById('autor-id')
-let lofiBtn = document.getElementById('lofi-id')
-let classicBtn = document.getElementById('classic-id')
-let deepBtn = document.getElementById('deeph-id')
-let ambientBtn = document.getElementById('ambient-id')
+let playRadioBtn = document.getElementById('play-radio')
+let pauseRadioBtn = document.getElementById('pause-radio')
+let video = document.querySelector('video')
+let source = document.querySelector('source')
+let lofiBox = document.getElementById('lofi-box-id')
+let deephBox = document.getElementById('deeph-box-id')
+let classicBox = document.getElementById('classic-box-id')
+let ambientBox = document.getElementById('ambient-box-id')
 // Teremos uma playlist de acordo com o tempo na regiao.
 // A descricao nos entrega mais detalhes sobre como esta o 
 // tempo naquela regiao. Como sendo a primeira versao 
@@ -33,23 +38,24 @@ const bgs = {
    'overcast clouds': "https://mylivewallpapers.com/wp-content/uploads/Fantasy/PREVIEW-Boat-on-Clouds.mp4",
    'mist': "https://mylivewallpapers.com/wp-content/uploads/Nature/PREVIEW-Calm-Lake.mp4",
    'moderate rain': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Living-Room-Chill.mp4",
+   'light rain': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-King-Wash.mp4",
    'heavy intensity rain': "https://mylivewallpapers.com/wp-content/uploads/City/PREVIEW-Rain-Street-Ghostwire-Tokyo.mp4",
    'very heavy rain': "https://mylivewallpapers.com/wp-content/uploads/City/PREVIEW-Raining-Streets-GTA5.mp4",
    'extreme rain': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Raining-Japanese-Village.mp4",
    'scattered clouds': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Room-with-Candles.mp4",
    'lofi': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Lo-Fi-Coffee-Shop.mp4",
    'deeph': "https://mylivewallpapers.com/wp-content/uploads/Fantasy/PREVIEW-Moon-Pool-Party.mp4",
-   'classic': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Study-Session.mp4"
-
+   'classic': "https://mylivewallpapers.com/wp-content/uploads/Lifestyle/PREVIEW-Study-Session.mp4",
+   'ambient': "https://mylivewallpapers.com/wp-content/uploads/City/PREVIEW-City-Ruins.mp4"
 }
 
 function criarPlaylist(playlist_id) {
    const playlistDiv = document.createElement("div")
    var conteudo =
       `
-      <iframe style="border-radius:12px" 
+      <iframe style="position: absolute; bottom: 0px; border-radius: 16px; margin: 30px;" id="spotify-player"
          src="https://open.spotify.com/embed/playlist/${playlist_id}?utm_source=generator&theme=0" 
-         width="100%" height="380" frameBorder="0" 
+         width="300" height="380" frameBorder="0" 
          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
          loading="lazy">
       </iframe>
@@ -92,6 +98,81 @@ const pegarTempo = () => {
       dadosLocalizacao.address.city + "&appid=2b425334302e4882839efd307f61b3fa")
 }
 
+function onYouTubeIframeAPIReady() {
+   playerDeeph = new YT.Player('deeph-id', {
+      height: '390',
+      width: '640',
+      videoId: 'GDQnA1LVCWA',
+      events: {
+      }
+   })
+
+   playerLofi = new YT.Player('lofi-id', {
+      height: '390',
+      width: '640',
+      videoId: 'ceqgwo7U28Y',
+      events: {
+      }
+   })
+
+   playerClassic = new YT.Player('classic-id', {
+      height: '390',
+      width: '640',
+      videoId: 'tvbyyL5wRGw',
+      events: {
+      }
+   })
+
+   playerAmbient = new YT.Player('ambient-id', {
+      height: '390',
+      width: '640',
+      videoId: 'AEkEjX4P_S0',
+      events: {
+      }
+   })
+
+}
+
+function startDeeph() {
+   playerLofi.stopVideo()
+   playerAmbient.stopVideo()
+   playerClassic.stopVideo()
+
+   playerDeeph.playVideo()
+   source.src = bgs['deeph']
+   video.load()
+}
+
+function startLofi() {
+   playerDeeph.stopVideo()
+   playerAmbient.stopVideo()
+   playerClassic.stopVideo()
+
+   playerLofi.playVideo()
+   source.src = bgs['lofi']
+   video.load()
+}
+
+function startClassic() {
+   playerLofi.stopVideo()
+   playerAmbient.stopVideo()
+   playerDeeph.stopVideo()
+
+   playerClassic.playVideo()
+   source.src = bgs['classic']
+   video.load()
+}
+
+function startAmbient() {
+   playerLofi.stopVideo()
+   playerDeeph.stopVideo()
+   playerClassic.stopVideo()
+
+   playerAmbient.playVideo()
+   source.src = bgs['ambient']
+   video.load()
+}
+
 function aplicarTema() {
    pegarDadosLocalizacao()
    pegarTempo()
@@ -100,16 +181,41 @@ function aplicarTema() {
    // Por enquanto acho interessante, quando pegar a localizacao novamente, 
    // "atualizar" a playlist
 
-   if (document.getElementById('bg-id').src.includes("Fisherman") == true) {
+   if (document.getElementById('spotify-player') == null) {
       corpo.appendChild(criarPlaylist(theme[tempoAtual][descTempo]))
    }
    else {
-      corpo.removeChild(corpo.firstElementChild)
+      document.getElementById('spotify-player').remove()
       corpo.appendChild(criarPlaylist(theme[tempoAtual][descTempo]))
    }
-   document.getElementById('bg-id').src = bgs[descTempo]
+   source.src = bgs[descTempo]
+   video.load()
 }
 
 gitBtn.onclick = () => {
    window.open("https://github.com/ansattz", "_blank")
+}
+
+pauseRadioBtn.onclick = () => {
+   playerLofi.stopVideo()
+   playerDeeph.stopVideo()
+   playerClassic.stopVideo()
+   playerAmbient.stopVideo()
+}
+
+playRadioBtn.onclick = () => {
+   if (lofiBox.checked == true) {
+      startLofi()
+   }
+   else if (deephBox.checked == true) {
+      startDeeph()
+   }
+
+   else if (classicBox.checked == true) {
+      startClassic()
+   }
+
+   else {
+      startAmbient()
+   }
 }
