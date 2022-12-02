a7b39b920c715a71ee9f4246f0698f907c33b4ae
ansattz <dvieiras@aol.com>
2022-12-02 01:11:41 -0300

    feat: bg video component

---
+++ src/app/components/background/background.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.sass']
+})
+export class BackgroundComponent {
+
+ urlVideo:string = "https://mylivewallpapers.com/wp-content/uploads/Nature/PREVIEW-Pond-Silent-Summer.mp4"  
+
+}
