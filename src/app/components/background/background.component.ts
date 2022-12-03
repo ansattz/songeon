0d12c49f3a112d873a882ddef1853e7e5ce618aa
ansattz <dvieiras@aol.com>
2022-12-03 17:09:09 -0300

    testing oninit for bg

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.sass']
 })
-export class BackgroundComponent {
+export class BackgroundComponent implements OnInit {
 
- urlVideo:string = "https://mylivewallpapers.com/wp-content/uploads/Nature/PREVIEW-Pond-Silent-Summer.mp4"  
+   urlVideo:string = ""  
+   ngOnInit(){
+      this.urlVideo = "https://mylivewallpapers.com/wp-content/uploads/Nature/PREVIEW-Pond-Silent-Summer.mp4"
+   }
 
 }
