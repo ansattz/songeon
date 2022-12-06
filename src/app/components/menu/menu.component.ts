ab2d023d928397f602e1109bcc8b56287a28fc13
ansattz <dvieiras@aol.com>
2022-12-06 17:54:44 -0300

    feat: get coords's user

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LocationService } from 'src/app/services/location/location.service';
+import { PositionService } from 'src/app/services/position/position.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.sass']
 })
 export class MenuComponent {
+
+   uLat!:number
+   uLng!:number
    showFiller:boolean = false;
+   userPosition: Array<any> = [this.uLng, this.uLat]
+   
+   constructor(private _pos: PositionService,
+              private loc: LocationService ){}
+
+   callPosition(){
+      this._pos.getPosition().then(resp => {
+         this.uLng = resp.lng
+         this.uLat = resp.lat
+         console.log(this.uLng)
+         console.log(this.uLat)
+      })
+   }
+
+// callLoc(){
+//    console.log(this.uLat)
+//    this.loc.getLoc() //this.loc.getLoc().subscribe((data) => console.log(data))
+// }
 }
