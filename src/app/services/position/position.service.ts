fe9def47dbbb6d4d7e8c0ce02141780917732735
ansattz <dvieiras@aol.com>
2022-12-08 17:01:55 -0300

    main service for: call apis and define the playlist

--- src/app/services/position/position.service.ts
+++ src/app/services/position/position.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
+import { LocationService } from '../location/location.service';
 
 @Injectable({
    providedIn: 'root'
 })
 export class PositionService {
 
-   constructor() { }
+   constructor(public loc: LocationService) { }
+   static uLat:number
+   static uLng:number
 
-   getPosition():Promise<any>{
-      return new Promise((resolve, reject) => {
+   async getPosition():Promise<any>{
+      return new Promise((resolve) => {
          navigator.geolocation.getCurrentPosition(resp => {
             resolve({lng: resp.coords.longitude,
-                    lat: resp.coords.latitude})
+                    lat: resp.coords.latitude},
+                   )
          })
       })
+   }
+
+   userPosition(){
+      this.getPosition().then(resp => {
+         PositionService.uLng = resp.lng
+         PositionService.uLat = resp.lat
+         this.loc.getLocation()         
+      })
+
    }
 }
