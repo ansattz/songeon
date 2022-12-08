14a49b5e579a28fbd3ba7ffc1dc65c80737bc027
ansattz <dvieiras@aol.com>
2022-12-08 17:05:38 -0300

    add: by now its fine to call this method --getThemes()-- here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ThemesService} from './services/themes/themes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'songeon';
+export class AppComponent implements OnInit{
+   constructor(public getTheme: ThemesService){}
+   ngOnInit(): void {
+      this.getTheme.getThemes()
+   }
 }
