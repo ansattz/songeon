498926be603745e11c36989751b8f25e80e9ed6c
ansattz <dvieiras@aol.com>
2022-12-11 03:02:13 -0300

    bg finished with interface docheck

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { ThemesService } from 'src/app/services/themes/themes.service';
+import { WeatherService } from 'src/app/services/weather/weather.service';
+
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.sass']
 })
-export class BackgroundComponent {
+export class BackgroundComponent implements DoCheck {
 
+   backgroundByPlaylist:string = "https://mylivewallpapers.com/wp-content/uploads/Nature/PREVIEW-Pond-Silent-Summer.mp4"
+
+   ngDoCheck(): void {
+      if(WeatherService.mainWe !== undefined){
+         this.backgroundByPlaylist = ThemesService.themesData["bg"][WeatherService.mainWe]
+      }
+   }
 }
