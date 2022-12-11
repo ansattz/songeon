86b924974e9c6f58cadfb8d55478094c00a3d316
ansattz <dvieiras@aol.com>
2022-12-10 21:03:11 -0300

    redoing background as a component

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.sass']
 })
-export class BackgroundComponent implements OnInit {
-
-   urlVideo:string = ""  
-   ngOnInit(){
-      this.urlVideo = "https://mylivewallpapers.com/wp-content/uploads/Nature/PREVIEW-Pond-Silent-Summer.mp4"
-   }
+export class BackgroundComponent {
 
 }
