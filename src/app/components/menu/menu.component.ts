207f8f242b8a08636fd513d2bad1d5969ebee4d4
ansattz <dvieiras@aol.com>
2022-12-12 20:43:26 -0300

    feature: favorite playlist

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlaylistService } from 'src/app/services/playlist/playlist.service';
 import { PositionService } from 'src/app/services/position/position.service';
 
 
@@ -10,10 +11,15 @@ import { PositionService } from 'src/app/services/position/position.service';
 export class MenuComponent {
    showFiller:boolean = false;
 
-   constructor(public pos: PositionService){}
+   constructor(public pos: PositionService,
+              private play: PlaylistService){}
 
    // Get position and set playist by local weather
    positionBtn(){
       this.pos.userPosition()
+   }
+
+   favoriteBtn(){
+      this.play.setFavoritePlaylist()
    }
 }
