a37fca96a669f2c4f29935def9002ec03c007573
ansattz <dvieiras@aol.com>
2022-12-12 18:04:03 -0300

    add: variable shareUrl$ with subject to subscribe on player component

--- src/app/services/playlist/playlist.service.ts
+++ src/app/services/playlist/playlist.service.ts
@@ -1,28 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { ThemesService } from 'src/app/services/themes/themes.service';
 import { WeatherService } from 'src/app/services/weather/weather.service';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistService {
 
-   static playlistUrl:string
-   static playlistsData:any
-   static myPlaylist:string
+   playlistUrl!:string
+   mainWeatherTheme:any
+   playlistID!:string
+   shareUrl$: Subject<string> = new Subject<string>
 
-   setPlaylist(){
-      PlaylistService.playlistUrl = "https://open.spotify.com/embed/playlist/" 
-      + PlaylistService.myPlaylist
+   setPlaylistUrl(){
+      this.playlistUrl = "https://open.spotify.com/embed/playlist/" 
+      + this.playlistID
       + "?utm_source=generator&theme=0"
-      console.log(PlaylistService.playlistUrl)
+      this.shareUrl$.next(this.playlistUrl)
    }
 
    async playlist(){
-      await new Promise(f => setTimeout(f, 1000))
-      PlaylistService.playlistsData = ThemesService.themesData[WeatherService.mainWe]
-      PlaylistService.myPlaylist = PlaylistService.playlistsData[WeatherService.descWe]
-      this.setPlaylist()
+      await new Promise(f => setTimeout(f, 1500))
+      this.mainWeatherTheme = ThemesService.themesData[WeatherService.mainKeyFromData]
+      this.playlistID = this.mainWeatherTheme[WeatherService.descriptionKeyFromData]
+      this.setPlaylistUrl()
    }
 }
