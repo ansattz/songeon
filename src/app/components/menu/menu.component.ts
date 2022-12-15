37273da4f42fec6e71962e716dc3b6661be3cc31
ansattz <dvieiras@aol.com>
2022-12-15 14:07:35 -0300

    feature: get playlist by search

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,7 +1,9 @@
-import { Component, ViewEncapsulation} from '@angular/core';
+import { Component, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { BackgroundService } from 'src/app/services/background/background.service';
 import { PlaylistService } from 'src/app/services/playlist/playlist.service';
 import { PositionService } from 'src/app/services/position/position.service';
+import {ThemesService} from 'src/app/services/themes/themes.service';
 
 
 @Component({
@@ -12,22 +14,44 @@ import { PositionService } from 'src/app/services/position/position.service';
 
 })
 export class MenuComponent {
-   showFiller:boolean = false;
+   modalRef?: BsModalRef
+   showFiller:boolean = false
+   searchInput:string = ""
 
-   constructor(public pos: PositionService,
+   constructor(private pos: PositionService,
               private play: PlaylistService,
-              private bg: BackgroundService){}
+              private bg: BackgroundService,
+              private modalService: BsModalService){}
 
    // Get position and set playist by local weather
    positionBtn(){
       this.pos.userPosition()
    }
 
+   // Get favorite playlist
    favoriteBtn(){
       this.play.setFavoritePlaylist()
    }
 
+   // Choose background
    chooseBackgroundBtn(path:string){
       this.bg.setUrlForBackground(path)
+   }
+
+   // Search playlists
+   openSearch(template: TemplateRef<any>){
+      this.modalRef = this.modalService.show(template)
+   }
+
+   searchPlaylistBtn(){
+      if(this.searchInput.length >= 48){
+         this.play.setSearchPlaylist(
+            String(new URL(this.searchInput).pathname.split('/').pop()))         
+         this.searchInput = ""
+         this.modalRef?.hide()
+      }else {
+         alert("Invalid URL")
+         this.searchInput = ""
+      }
    }
 }
